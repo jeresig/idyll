c828c646f3e1d2b6f3a405c7bae0f3fdf2e7b850
John Resig <jeresig@gmail.com>
2015-05-02 13:43:01 -0400

    Fix up result saving, get the upgrade script fully working.

--- utils/upgrade-data.js
+++ utils/upgrade-data.js
@@ -5,29 +5,34 @@ var async = require("async");
 var mongoose = require("mongoose");
 var ArgumentParser = require("argparse").ArgumentParser;
 
+// Load in ENV variables
+require("dotenv").load();
+
+mongoose.connect(process.env.MONGO_URL);
+
 // ARG PARSER
-var parser = new ArgumentParser({
+var argparser = new ArgumentParser({
     version: "0.0.1",
     addHelp: true
 });
 
-parser.addArgument(["cropDataDir"], {
-    help: "The directory holding the MongoDB json files from Idyll."
+argparser.addArgument(["--user"], {
+    help: "The user ID to use to authenticate with Idyll.",
+    defaultValue: process.env.IDYLL_USER,
+    dest: "user"
 });
-var args = parser.parseArgs();
 
-var BASE_DATA_DIR = path.resolve(process.env.BASE_DATA_DIR, args.source);
+argparser.addArgument(["cropDataDir"], {
+    help: "The directory holding the MongoDB export json files from Idyll."
+});
 
-var imagesDir = path.resolve(BASE_DATA_DIR, "images");
-var scaledDir = path.resolve(BASE_DATA_DIR, "scaled");
-var thumbsDir = path.resolve(BASE_DATA_DIR, "thumbs");
-var croppedDir = path.resolve(BASE_DATA_DIR, "cropped");
+var args = argparser.parseArgs();
 
 var images = require(path.resolve(args.cropDataDir, "images.json"));
 var selections = require(path.resolve(args.cropDataDir, "selections.json"));
 
 // Load models
-var modelsDir = path.resolve(__dirname, "../app/models");
+var modelsDir = path.resolve(__dirname, "../server/app/models");
 
 fs.readdirSync(modelsDir).forEach(function (file) {
     if (~file.indexOf(".js")) {
@@ -40,10 +45,12 @@ var Result = mongoose.model("Result");
 var Task = mongoose.model("Task");
 var User = mongoose.model("User");
 
-var userID = "creator";
+var userID = args.user;
 var jobs = {};
 var tasks = [];
 var results = [];
+
+console.log(images.length)
 
 async.eachSeries(images, function(image, callback) {
     var match = /^([^\/]+)\/(.*)$/.exec(image.scaled.file);
@@ -62,6 +69,7 @@ async.eachSeries(images, function(image, callback) {
     var job = jobs[source];
 
     var task = new Task({
+        creator: userID,
         job: job._id,
         assigned: [],
         files: [{
@@ -75,7 +83,7 @@ async.eachSeries(images, function(image, callback) {
         }]
     });
 
-    var selections = [];
+    var matchedSelections = [];
 
     var selectionIDs = image.selections.map(function(item) {
         return item.$oid;
@@ -85,19 +93,17 @@ async.eachSeries(images, function(image, callback) {
         var id = selections[i]._id.$oid;
 
         if (selectionIDs.indexOf(id) >= 0) {
-            selections.push(selections[i]);
+            matchedSelections.push(selections[i]);
         }
     }
 
-    var result;
-
-    if (selections.length > 0) {
-        result = new Result({
+    matchedSelections.forEach(function(selection) {
+        var result = new Result({
             task: task._id,
             user: userID,
-            started: selection.started,
-            completed: selection.completed,
-            results: selections.map(function(selection) {
+            started: new Date(selection.started.$date),
+            completed: new Date(selection.completed.$date),
+            results: selection.selections.map(function(selection) {
                 return {
                     height: selection.height,
                     width: selection.width,
@@ -108,24 +114,37 @@ async.eachSeries(images, function(image, callback) {
         });
 
         task.results.push(result._id);
-    }
+        results.push(result);
+
+        console.log("Making result", result._id);
+    });
 
     console.log("Saving task:", task._id);
-    //task.save(callback);
-    callback();
-}, function() {
-    console.log("Saving jobs...");
+    task.save(callback);
+
+}, function(err) {
+    if (err) {
+        console.error(err);
+    }
+
+    console.log("Saving jobs...", Object.keys(jobs).length);
     async.eachSeries(Object.keys(jobs), function(jobName, callback) {
-        //jobs[jobName].save(callback);
-        callback();
-    }, function() {
+        jobs[jobName].save(callback);
+    }, function(err) {
+        if (err) {
+            console.error(err);
+        }
+
         console.log("Saved.");
-        console.log("Saving results...");
+        console.log("Saving results...", results.length);
 
         async.eachSeries(results, function(result, callback) {
-            //result.save(callback);
-            callback();
-        }, function() {
+            result.save(callback);
+        }, function(err) {
+            if (err) {
+                console.error(err);
+            }
+
             console.log("Finished.");
             process.exit(0);
         });
